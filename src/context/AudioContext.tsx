826be808580a4f605fab06007126c1953aee5d48
Yanluis F <32645451+Jacxk@users.noreply.github.com>
2025-04-01 20:58:53 -0400

    refactor: remove setCurrentAudio and play audio on every click

--- src/context/AudioContext.tsx
+++ src/context/AudioContext.tsx
@@ -12,7 +12,6 @@ import {
 
 interface AudioContextType {
   currentAudio: HTMLAudioElement | null;
-  setCurrentAudio: (audio: HTMLAudioElement | null) => void;
   audioRef: RefObject<HTMLAudioElement>;
   play: () => void;
 }
@@ -27,23 +26,17 @@ export const AudioProvider = ({ children }: { children: ReactNode }) => {
 
   const play = () => {
     if (audioRef.current) {
-      if (currentAudio && currentAudio !== audioRef.current) {
+      if (currentAudio) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
       }
-      if (audioRef.current.paused) {
-        void audioRef.current.play();
-        setCurrentAudio(audioRef.current);
-      } else {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-        setCurrentAudio(null);
-      }
+      audioRef.current.play();
+      setCurrentAudio(audioRef.current);
     }
   };
 
   const value = useMemo(
-    () => ({ currentAudio, audioRef, play, setCurrentAudio }),
+    () => ({ currentAudio, audioRef, play }),
     [currentAudio],
   );
 
