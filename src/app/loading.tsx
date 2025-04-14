e9dfa4684cfcba29a2fce90292ef5e9d8465bb71
Yanluis F <32645451+Jacxk@users.noreply.github.com>
2025-04-14 11:30:02 -0400

    feat: add loading component for user guilds page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="flex h-full w-full items-center justify-center text-2xl font-bold text-primary/50">
+      <span>Loading...</span>
+    </div>
+  );
+}
