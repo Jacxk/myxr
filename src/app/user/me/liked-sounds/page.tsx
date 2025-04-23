dd9213edc556a5761fb6fdf57373f08db3f6fb62
Yanluis F <32645451+Jacxk@users.noreply.github.com>
2025-04-22 22:13:34 -0400

    feat: add message for users with no liked sounds

--- src/app/user/me/liked-sounds/page.tsx
+++ src/app/user/me/liked-sounds/page.tsx
@@ -5,6 +5,13 @@ import { api } from "~/trpc/server";
 export default async function () {
   const sounds = await api.user.likedSounds();
 
+  if (sounds.length === 0)
+    return (
+      <div className="flex w-full flex-wrap items-center justify-center">
+        <span>You have not liked any sounds yet. Why is that?</span>
+      </div>
+    );
+
   return (
     <SoundsGrid sm={4} md={5} lg={6} xl={8}>
       {sounds.map((sound) => (
