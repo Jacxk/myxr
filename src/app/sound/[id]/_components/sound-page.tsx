04846cd7b499fc21f41e809564716bfac43b0fe9
Jack <32645451+Jacxk@users.noreply.github.com>
2025-05-01 12:13:57 -0400

    refactor: adjust spacing in SoundPage layout for improved UI consistency

--- src/app/sound/[id]/_components/sound-page.tsx
+++ src/app/sound/[id]/_components/sound-page.tsx
@@ -53,8 +53,8 @@ function ActionButtons({ id, sound, isPreview }: SoundPageProps) {
 
 export function SoundPage({ id, sound, isPreview }: Readonly<SoundPageProps>) {
   return (
-    <div className="flex w-full flex-col gap-4">
-      <div className="flex w-full flex-row gap-4">
+    <div className="flex w-full flex-col gap-6">
+      <div className="flex w-full flex-row gap-10">
         <div className="flex shrink-0">
           <SoundEmoji emoji={sound.emoji} />
         </div>
