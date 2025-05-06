02d159d369483ffb05468a112383fd9fc64116bd
Jack <32645451+Jacxk@users.noreply.github.com>
2025-05-06 15:13:12 -0400

    feat: add Google AdSense with dynamic script loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,11 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 import NextTopLoader from "nextjs-toploader";
 import { PostHogProvider } from "~/components/PostHogProvider";
 import { Toaster } from "~/components/ui/sonner";
+import { env } from "~/env";
 import { TRPCReactProvider } from "~/trpc/react";
 import { HydrateClient } from "~/trpc/server";
 import { Footer } from "./_components/footer";
@@ -23,6 +25,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={GeistSans.variable} suppressHydrationWarning>
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_ADSENSE_KEY}`}
+        crossOrigin="anonymous"
+        strategy="lazyOnload"
+      />
       <body>
         <PostHogProvider>
           <TRPCReactProvider>
