f48615937ef3793f74fb169ac78e6d72a1f5dcc9
Jack <32645451+Jacxk@users.noreply.github.com>
2025-05-19 21:54:10 -0400

    fix: conditionally render guild images in user guilds layout

--- src/app/user/me/guilds/layout.tsx
+++ src/app/user/me/guilds/layout.tsx
@@ -13,19 +13,21 @@ export default async function Layout({
   return (
     <div className="flex w-full flex-col">
       <div className="flex flex-row gap-1 overflow-x-auto border-b sm:justify-center">
-        {guilds?.map((guild, i) => (
+        {guilds?.map((guild) => (
           <TabLink
             className="rounded-b-none"
             key={guild.id}
             path={`/user/me/guilds/${guild.id}`}
           >
             <Avatar title={guild.name}>
-              <AvatarImage
-                className="rounded-full"
-                src={guild.image + "?size=32"}
-                alt={guild.name}
-                useNextImage
-              />
+              {guild.image && (
+                <AvatarImage
+                  className="rounded-full"
+                  src={guild.image + "?size=32"}
+                  alt={guild.name}
+                  useNextImage
+                />
+              )}
               <AvatarFallback className="flex items-center justify-center text-xs">
                 {guild.name.slice(0, 1).toUpperCase()}
               </AvatarFallback>
