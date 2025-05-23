eee7dcdf793fc7098c772718d8c06c6d12b8de5f
Jack <32645451+Jacxk@users.noreply.github.com>
2025-05-22 23:07:33 -0400

    refactor: simplify Tab component with useSelectedLayoutSegment and add current tab label on mobile layout

--- src/app/user/me/_components/tab-link.tsx
+++ src/app/user/me/_components/tab-link.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Castle, Flag, Heart, Trash, Volume2 } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useSelectedLayoutSegment } from "next/navigation";
+import React from "react";
 import { cn } from "~/lib/utils";
 
 export function TabLink({
@@ -13,8 +15,9 @@ export function TabLink({
   path: string;
   className?: string;
 }>) {
-  const pathname = usePathname();
-  const isActive = pathname.startsWith(path);
+  const segment = useSelectedLayoutSegment();
+  const splitPath = path.split("/");
+  const isActive = segment === splitPath[splitPath.length - 1];
 
   return (
     <Link
@@ -24,8 +27,71 @@ export function TabLink({
         isActive ? "bg-secondary/50 text-primary/75" : "text-primary/50",
         className,
       )}
+      prefetch={false}
     >
       {children}
     </Link>
+  );
+}
+
+interface UserTabItem {
+  path: string;
+  label: string;
+  icon: React.JSX.Element;
+  className?: string;
+}
+
+export function UserTabsAccount() {
+  const segment = useSelectedLayoutSegment();
+
+  const tabs: UserTabItem[] = [
+    {
+      path: "sounds",
+      label: "My Sounds",
+      icon: <Volume2 className="shrink-0" />,
+    },
+    {
+      path: "liked-sounds",
+      label: "Liked Sounds",
+      icon: <Heart className="shrink-0" />,
+    },
+    {
+      path: "guilds",
+      label: "Guilds",
+      icon: <Castle className="shrink-0" />,
+    },
+    {
+      path: "reports",
+      label: "Reports",
+      icon: <Flag className="shrink-0" />,
+    },
+    {
+      path: "delete-account",
+      label: "Delete Account",
+      icon: <Trash />,
+      className: "text-red-500 hover:bg-red-500/20",
+    },
+  ];
+
+  const activeTab = tabs.find((tab) => tab.path === segment);
+
+  return (
+    <>
+      <div className="flex flex-row justify-center gap-2 sm:w-1/2 sm:flex-col sm:justify-start lg:w-1/4">
+        {tabs.map((tab) => (
+          <TabLink
+            key={tab.path}
+            path={`/user/me/${tab.path}`}
+            className={cn("flex flex-row gap-2", tab.className)}
+          >
+            {tab.icon}
+            <span className="hidden sm:block">{tab.label}</span>
+          </TabLink>
+        ))}
+      </div>
+      <span className="flex-1 text-center text-lg sm:hidden">
+        {activeTab?.label}
+      </span>
+    </>
   );
 }
--- src/app/user/me/layout.tsx
+++ src/app/user/me/layout.tsx
@@ -1,31 +1,6 @@
-import { Castle, Flag, Heart, Trash, Volume2 } from "lucide-react";
 import { redirect } from "next/navigation";
-import { type ReactNode } from "react";
 import { getServerSession } from "~/lib/auth";
-import { cn } from "~/lib/utils";
-import { TabLink } from "./_components/tab-link";
-
-function Tab({
-  path,
-  label,
-  icon,
-  className,
-}: {
-  path: string;
-  className?: string;
-  label: string;
-  icon: ReactNode;
-}) {
-  return (
-    <TabLink
-      path={`/user/me${path}`}
-      className={cn("flex flex-row gap-2", className)}
-    >
-      {icon}
-      <span className="hidden sm:block">{label}</span>
-    </TabLink>
-  );
-}
+import { UserTabsAccount } from "./_components/tab-link";
 
 export default async function Layout({
   children,
@@ -38,34 +13,7 @@ export default async function Layout({
 
   return (
     <div className="flex flex-col gap-4 sm:flex-row">
-      <div className="flex flex-row justify-center gap-2 sm:w-1/2 sm:flex-col sm:justify-start lg:w-1/4">
-        <Tab
-          path="/sounds"
-          label="My Sounds"
-          icon={<Volume2 className="shrink-0" />}
-        />
-        <Tab
-          path="/liked-sounds"
-          label="Liked Sounds"
-          icon={<Heart className="shrink-0" />}
-        />
-        <Tab
-          path={`/guilds`}
-          label="Guilds"
-          icon={<Castle className="shrink-0" />}
-        />
-        <Tab
-          path="/reports"
-          label="Reports"
-          icon={<Flag className="shrink-0" />}
-        />
-        <Tab
-          path="/delete-account"
-          label="Delete Account"
-          icon={<Trash />}
-          className="text-red-500 hover:bg-red-500/20"
-        />
-      </div>
+      <UserTabsAccount />
       {children}
     </div>
   );
