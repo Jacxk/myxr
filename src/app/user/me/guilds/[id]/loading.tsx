82e7d82572c16890912302b9d500861ba561faf8
Jack <32645451+Jacxk@users.noreply.github.com>
2025-05-26 22:47:50 -0400

    style: improve loading component layout with responsive design adjustments

--- src/app/user/me/guilds/[id]/loading.tsx
+++ src/app/user/me/guilds/[id]/loading.tsx
@@ -7,8 +7,11 @@ export default function Loading() {
       <Skeleton className="mx-2 h-8 w-8 self-end" />
       <div className="divide-y">
         {Array.from({ length: 5 }).map((_, i) => (
-          <div key={i} className="flex items-center justify-between p-4">
-            <div className="flex items-start gap-4">
+          <div
+            key={i}
+            className="flex flex-col gap-4 p-4 sm:flex-row sm:items-center sm:justify-between"
+          >
+            <div className="flex gap-4">
               <Skeleton className="h-[70px] w-[70px] rounded-lg" />
               <div className="flex flex-col gap-2">
                 <Skeleton className="h-5 w-32" />
@@ -20,7 +23,7 @@ export default function Loading() {
                 </div>
               </div>
             </div>
-            <div className="flex items-center gap-2">
+            <div className="flex gap-2 self-end">
               <Skeleton className="h-8 w-8" />
               <Skeleton className="h-8 w-8" />
               <Skeleton className="h-8 w-8" />
